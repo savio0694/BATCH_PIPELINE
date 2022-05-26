017f5f9639ba638f0e7a976ab3194facc6a8274b
Leander0694 <leandersavio@live.com>
2022-05-26 10:36:16 +0100

    added 5

--- rename_script.py
+++ rename_script.py
@@ -3,7 +3,7 @@ from google.cloud import storage
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/opt/airflow/credentials/keys.json'
 
 bucket_name = "cleanbucket0694"
-#change1234
+#change12345
 
 def list_blobs(bucket_name):
     
