4295335a7979fb5d831be7a583fe8724b4083cff
Leander0694 <leandersavio@live.com>
2022-05-26 10:21:02 +0100

    new branch

--- rename_script.py
+++ rename_script.py
@@ -3,7 +3,7 @@ from google.cloud import storage
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/opt/airflow/credentials/keys.json'
 
 bucket_name = "cleanbucket0694"
-#change12
+#change123
 
 def list_blobs(bucket_name):
     
